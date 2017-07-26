710890e97208d8c0542861b524c28816a76380d4
kent <kentkawashima@gmail.com>
2017-07-26 14:52:10 +0900

    Adds documentation to exported variables

--- constants.go
+++ constants.go
@@ -5,9 +5,12 @@ import (
 	"strconv"
 )
 
+// Bases lists the allowed nucleotide base characters in the SequenceArrays or SequenceAlignments.
 var Bases = [4]string{ "T", "C", "A", "G" }
+
+// Codons lists the allowed codon strings in CodonAlignments.
 var Codons = [64]string{}
-i = 0
+var i = 0
 for _, a := range Bases {
 	for _, b := range Bases {
 		for _, c := range Bases {
@@ -16,7 +19,11 @@ for _, a := range Bases {
 		}
 	}
 }
+
+// StopCodons lists the codons that yield to a termination signal.
 var StopCodons = [3]string{ "TGA", "TAG", "TAA" }
+
+// AminoAcids lists the allowed single-character amino acid representations in SequenceArrays or SequenceAlignments.
 var AminoAcids = [20]string{
 	"A",
 	"R",
@@ -53,6 +60,7 @@ for i := range Codons {
 	GeneticCode[Codons[i]] = transl[i]
 }
 
+// DegenerateBases lists IUPAC characters to represent degenerate bases and the nucleotide bases they represent.
 var DegenerateBases = map[string][]string{
 	"W": []string{ "A", "T" },  // 2-FOLD
 	"S": []string{ "C", "G" },
@@ -67,19 +75,24 @@ var DegenerateBases = map[string][]string{
 	"N": []string{ "A", "C", "G", "T" },  // 4-FOLD
 }
 
+// AAFold lists the number of different codons that code for a particular amino acid.
 var AAFold = make(map[string]int)
 for _, v := range GeneticCode {
 	AAFold[v]++
 }
 
+// CodonFold lists which fold category a particular codon belongs to. 
+// This count is based on the number of different codons that result in particular amino acid.
 var CodonFold = make(map[string]int)
 for k, v := range GeneticCode {
 	CodonFold[k] : AAFold[v]
 }
 
+// MSA is a struct for a multiple sequence alignment datatype
 type MSA struct {
 	ids []string
 	alignment [][]string
 }
 
+// SequenceTypes lists the possible sequence types in a SequenceArray or SequenceAlignment
 var SequenceTypes = [3]string{ "nucl", "prot", "cod" }
